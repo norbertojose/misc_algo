b75b861995197c10dbd9a2f4f7094e8cf891b9ec
jose.laverde <jose.la.verde@continental-corporation.com>
2016-11-01 18:08:24 -0600

    implementing returning the nodes by same depth

--- src/trees/MyBinaryTreeTest.java
+++ src/trees/MyBinaryTreeTest.java
@@ -1,5 +1,8 @@
 package trees;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 
@@ -8,16 +11,34 @@ public class MyBinaryTreeTest {
 	private static Node<Integer> binaryTree;
 	
 	public static void main(String[] args){
-		generateRandomBinaryTreeWithIntegerValues(10);
+		if(args.length>0)
+			System.out.print(args[0]);
+		
+		
+		generateRandomBinaryTreeWithIntegerValues(Integer.parseInt(args[0]));
+		binaryTree.setDepth(0);
+		//binaryTree.printDepth();
+		System.out.println("MAX DEPTH:" + binaryTree.getMaxDepth());
+		HashMap<Integer,ArrayList<Node<Integer>>> map = binaryTree.getNodesByDepth(binaryTree);
+		for(int key: map.keySet())
+		{
+			System.out.println("DEPTH: " + key);
+			System.out.print("nodes: ");
+			for(Node<Integer> node:map.get(key))
+			{
+				System.out.print(node.getValue()+"---");
+			}
+			System.out.println("");
+		}
 	}
 	
 	public static void generateRandomBinaryTreeWithIntegerValues(int numberOfNodes){
 		Random rnd = new Random(0);
 		binaryTree = new Node<Integer>(0);
 		for (int i = 1; i < numberOfNodes; i++) {
-			binaryTree.appendInOrderNode(new Node<Integer>(i));
+			binaryTree.createRandomBinaryTree(new Node<Integer>(i));
 		}
 		
-		binaryTree.printInOrderTraversal();
+		//binaryTree.printInOrderTraversal();
 	}
 }
--- src/trees/Node.java
+++ src/trees/Node.java
@@ -1,9 +1,15 @@
 package trees;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Node<T extends Object> {
 	private Node<T> nextRigth=null;
 	private Node<T> nextLeft=null;
 	private T value;
+	private int depth=0;
 
 	
 	public Node(T value, Node<T> nextRigth,Node<T> nextLeft){
@@ -15,8 +21,69 @@ public class Node<T extends Object> {
 		this.value	   = value;
 	}
 	
+	public void setDepth(int depth){
+		this.depth = depth;
+		//System.out.println("Node " + this.value +"  Has depth: " + this.depth);
+		if(this.nextLeft!=null)
+			this.nextLeft.setDepth(depth+1);
+		if(this.nextRigth!=null)
+			this.nextRigth.setDepth(depth + 1);
+	}
+	
+	public void printDepth()
+	{
+		System.out.println("Getting in");
+		System.out.println("Node " + this.value +"  Has depth: " + this.depth);
+		if(this.nextLeft!=null)
+			this.nextLeft.printDepth();
+		if(this.nextRigth!=null)
+			this.nextRigth.printDepth();
+			
+	}
+	
+	public  HashMap<Integer,ArrayList<Node<T>>> getNodesByDepth(Node<T> rootNode){
+		HashMap<Integer,ArrayList<Node<T>>> toRet = new HashMap<Integer,ArrayList<Node<T>>>();		
+		rootNode.setDepth(0);
+		addNodeToDepth(toRet,rootNode);
+		return toRet;
+	}
+	
+	private void addNodeToDepth(HashMap<Integer, ArrayList<Node<T>>> depthHash, Node<T> node) {
+		System.out.println("adding node to depth: " +node.getDepth()+"with value: " + node.getValue());
+		int currentDepth = node.getDepth();
+		if(depthHash.containsKey(currentDepth))
+		{
+			depthHash.get(currentDepth).add(node);
+		}else
+		{
+			ArrayList<Node<T>> listAtDepth = new ArrayList<Node<T>>();
+			listAtDepth.add(node);
+			depthHash.put(currentDepth,listAtDepth);
+		}
+		if(node.nextLeft!=null)
+			addNodeToDepth(depthHash,node.nextLeft);
+		if(node.nextRigth!=null)
+			addNodeToDepth(depthHash,node.nextRigth);
+	}
+
+	public int getDepth() {
+		return this.depth;
+	}
+
+	public int getMaxDepth(){
+
+		int maxDepth = this.depth;
+		System.out.println("calculating max depth:" + this.value);
+		if(this.nextLeft!=null)
+			maxDepth = Math.max(this.depth,this.nextLeft.getMaxDepth());
+		
+		if(this.nextRigth!=null)
+			maxDepth = Math.max(maxDepth,this.nextRigth.getMaxDepth());
+		return maxDepth;
+	}
+	
 	public void printInOrderTraversal(){
-		System.out.println("Node Value is: "+value.toString());
+		System.out.println("Node"+value.toString());
 		if(this.nextLeft != null)
 		{
 			System.out.print("LEFT");
@@ -45,23 +112,25 @@ public class Node<T extends Object> {
 		this.nextLeft = nextLeft;
 	}
 	
-	public void appendInOrderNode(Node<T> node){
+	public void createRandomBinaryTree(Node<T> node){
 		if(this.nextLeft==null){
-			System.out.println("appending to the left of     " + this.getValue() + "   node:   " + node.getValue());
+			//System.out.println("appending to the left of     " + this.getValue() + "   node:   " + node.getValue());
 			this.nextLeft= node;
 		}
 		else if(this.nextRigth==null){
-			System.out.println("appending to the right of    " + this.getValue() + "    node: " + node.getValue());
+			//System.out.println("appending to the right of    " + this.getValue() + "    node: " + node.getValue());
 			this.nextRigth = node;
 		}
 		else if(this.nextLeft.getNextLeft() == null)
-			this.nextLeft.appendInOrderNode(node);
+			this.nextLeft.createRandomBinaryTree(node);
 		else if(this.nextRigth.getNextLeft() == null)
-			this.nextRigth.appendInOrderNode(node);
+			this.nextRigth.createRandomBinaryTree(node);
 		else if(this.nextLeft.getNextRigth() == null)
-			this.nextLeft.appendInOrderNode(node);
+			this.nextLeft.createRandomBinaryTree(node);
 		else if(this.nextRigth.getNextRigth() == null)
-			this.nextRigth.appendInOrderNode(node);
+			this.nextRigth.createRandomBinaryTree(node);
+		else
+			this.nextLeft.createRandomBinaryTree(node);
 	}
 	
 	public T getValue() {
